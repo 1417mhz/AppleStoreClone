a3fabcb8aac65ddeba381525411f8055501dd0cc
1417mhz <ysw99@kakao.com>
2023-12-07 21:47:16 +0900

    주문 기능 업데이트 트랜잭션 예외 처리 주문 시 구매자의 잔고에서 차감되도록 구현 주문 취소 시 구매자의 잔고 원복 구현

--- src/main/java/com/ysw/applestoreclone/service/OrderService.java
+++ src/main/java/com/ysw/applestoreclone/service/OrderService.java
@@ -13,15 +13,57 @@ import java.util.List;
 public class OrderService {
     public void createNewOrder(OrderBean orderBean) throws SQLException {
         Connection conn = DBConn.getDBConn();
-        String query = "INSERT INTO product_order(product_title, order_price, buyer) values(?, ?, ?)";
-        PreparedStatement pstmt = conn.prepareStatement(query);
-        pstmt.setString(1, orderBean.getProductTitle());
-        pstmt.setInt(2, orderBean.getOrderPrice());
-        pstmt.setString(3, orderBean.getBuyer());
-        pstmt.executeUpdate();
-        conn.close();
-        pstmt.close();
-        System.out.println("** 결제 성공 **");
+        conn.setAutoCommit(false);
+        String orderQuery = "INSERT INTO product_order(product_title, order_price, buyer) values(?, ?, ?)";
+        String updateQuery = "UPDATE user SET user_balance = user_balance - ? WHERE user_id = ?";
+        try (
+                PreparedStatement pstmtOrder = conn.prepareStatement(orderQuery);
+                PreparedStatement pstmtUpdate = conn.prepareStatement(updateQuery);
+        ) {
+            pstmtOrder.setString(1, orderBean.getProductTitle());
+            pstmtOrder.setInt(2, orderBean.getOrderPrice());
+            pstmtOrder.setString(3, orderBean.getBuyer());
+            pstmtOrder.executeUpdate();
+
+            pstmtUpdate.setInt(1, orderBean.getOrderPrice());
+            pstmtUpdate.setString(2, orderBean.getBuyer());
+            pstmtUpdate.executeUpdate();
+
+            conn.commit();
+            System.out.println("** 결제 성공 **");
+        } catch (SQLException e) {
+            conn.rollback();
+        } finally {
+            conn.setAutoCommit(true);
+            conn.close();
+        }
+    }
+
+    public void cancelOrder(int orderNo) throws SQLException {
+        Connection conn = DBConn.getDBConn();
+        conn.setAutoCommit(false);
+        String cancelQuery = "UPDATE product_order SET order_state = 'canceled', cancel_date = CURRENT_TIMESTAMP WHERE order_no = ?";
+        String updateQuery = "UPDATE user SET user_balance = user_balance + ? WHERE user_id = ?";
+        try (
+                PreparedStatement pstmtCancel = conn.prepareStatement(cancelQuery);
+                PreparedStatement pstmtUpdate = conn.prepareStatement(updateQuery);
+        ) {
+            pstmtCancel.setInt(1, orderNo);
+            pstmtCancel.executeUpdate();
+
+            OrderBean order = getOrderByOrderNo(orderNo);
+            pstmtUpdate.setInt(1, order.getOrderPrice());
+            pstmtUpdate.setString(2, order.getBuyer());
+            pstmtUpdate.executeUpdate();
+
+            conn.commit();
+            System.out.println("** 결제 취소 성공 **");
+        } catch (SQLException e) {
+            conn.rollback();
+        } finally {
+            conn.setAutoCommit(true);
+            conn.close();
+        }
     }
 
     public List<OrderBean> getAllOrder() throws SQLException {
@@ -65,21 +107,28 @@ public class OrderService {
             orderBean.setOrderCancelDate(rs.getString("cancel_date"));
             orderList.add(orderBean);
         }
-        System.out.println("orderList = " + orderList);
         conn.close();
         pstmt.close();
         rs.close();
         return orderList;
     }
 
-    public void cancelOrder(int orderNo) throws SQLException {
+    public OrderBean getOrderByOrderNo(int orderNo) throws SQLException {
         Connection conn = DBConn.getDBConn();
-        String query = "UPDATE product_order SET cancel_date = CURRENT_TIMESTAMP WHERE order_no = ?";
+        String query = "SELECT * FROM product_order WHERE order_no = ?";
         PreparedStatement pstmt = conn.prepareStatement(query);
         pstmt.setInt(1, orderNo);
-        pstmt.executeUpdate();
+        ResultSet rs = pstmt.executeQuery();
+        OrderBean orderBean = new OrderBean();
+        while (rs.next()) {
+            orderBean.setOrderNo(rs.getString("order_no"));
+            orderBean.setProductTitle(rs.getString("product_title"));
+            orderBean.setOrderPrice(Integer.parseInt(rs.getString("order_price")));
+            orderBean.setBuyer(rs.getString("buyer"));
+        }
+        rs.close();
         conn.close();
         pstmt.close();
-        System.out.println("** 결제 취소 성공 **");
+        return orderBean;
     }
 }
